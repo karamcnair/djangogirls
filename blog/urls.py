8416da2c2251eb6ec5b8b36c2e124ad8c65cef4d
Kara McNair <github@karamcnair.com>
2015-12-25 11:38:47 -0800

    modified templates to display posts from db, added second view

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.post_list, name='post_list')
+	url(r'^$', views.post_list, name='post_list'),
+	url(r'^all_posts$', views.all_posts, name='all_posts')
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,7 @@ from .models import Post
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+def all_posts(request):
+	posts = Post.objects.all().order_by('-author')
+	return render(request, 'blog/post_list.html', {'posts': posts})
