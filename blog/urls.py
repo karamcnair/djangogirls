2b7992a4514b63df0b60254fe68ffedd19bcd0c4
Kara McNair <github@karamcnair.com>
2015-12-25 12:15:03 -0800

    added view & template for detailed post, CSS

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
-	url(r'^all_posts$', views.all_posts, name='all_posts')
+	url(r'^all_posts$', views.all_posts, name='all_posts'),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -10,3 +10,7 @@ def post_list(request):
 def all_posts(request):
 	posts = Post.objects.all().order_by('-author')
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
