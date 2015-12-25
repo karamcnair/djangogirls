7a0134ca98b50b780b471ebe69b79d482f0917e6
Kara McNair <github@karamcnair.com>
2015-12-25 12:35:59 -0800

    added views to create/edit posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
 	url(r'^all_posts$', views.all_posts, name='all_posts'),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name="post_detail"),
+	url(r'^post/new/$', views.post_new, name="post_new"),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name="post_edit"),
+
 ]
